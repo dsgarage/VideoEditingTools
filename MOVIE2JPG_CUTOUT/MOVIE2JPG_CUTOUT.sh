3a9e39b981c5d4bcc002ec608da1cb423ddf1944
Daisuke Tsukada <dsgarage@gmail.com>
2024-01-07 15:45:07 +0900

    オプション機能追加

--- MOVIE2JPG_CUTOUT/MOVIE2JPG_CUTOUT.sh
+++ MOVIE2JPG_CUTOUT/MOVIE2JPG_CUTOUT.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+overwrite=0
+mode=""
+random_position=0
+
+# オプションを取得
+while getopts "rf:o1:2:3:4:" option; do
+    case $option in
+        r) mode="random"
+           random_position="$OPTARG";;
+        f) mode="fixed";;
+        o) overwrite=1;;
+    esac
+done
+
 # ファイル名が"RC"から始まる動画ファイルを検索
 for file in RC*.m4v
 do
@@ -9,25 +23,37 @@ do
     total_duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
     total_duration=${total_duration%.*}
 
-    # 4つのランダムな時間点を生成
-    for i in {1..4}
-    do
-        if [ $total_duration -gt 30 ]; then
-            let "random_times[i] = $RANDOM % ($total_duration - 30) + 30"
+    # オプションに応じた画像の切り出し
+    if [ "$mode" = "random" ]; then
+        if [ "$random_position" -ge 1 ] && [ "$random_position" -le 4 ]; then
+            # 動画を4等分し、指定されたポジションの前後3秒のランダム位置を計算
+            let "position = $total_duration / 4 * $random_position"
+            let "start_position = $position - 3"
+            let "end_position = $position + 3"
+            let "random_time = $start_position + $RANDOM % ($end_position - $start_position)"
+            output_file="RC$(printf "%02d" $random_position).jpg"
         else
-            random_times[i]=30
+            # 通常のランダム切り出し
+            let "random_time = $RANDOM % ($total_duration - 30) + 30"
+            output_file="RC_random.jpg"
         fi
-    done
-
-    # 時間点を昇順に並べ替え
-    IFS=$'\n' sorted_times=($(sort -n <<<"${random_times[*]}"))
-    unset IFS
-
-    # 昇順に並べ替えた時間で画像を切り出し
-    for i in {0..3}
-    do
-        ffmpeg -ss ${sorted_times[i]} -i "$file" -frames:v 1 "RC$(printf "%02d" $((i + 1))).jpg"
-    done
+        if [ -f "$output_file" ] && [ $overwrite -eq 0 ]; then
+            echo "File $output_file exists, skipping"
+            continue
+        fi
+        ffmpeg -y -ss $random_time -i "$file" -frames:v 1 "$output_file"
+    elif [ "$mode" = "fixed" ]; then
+        # 4等分した位置で画像を切り出し
+        for i in 1 2 3 4; do
+            let "time = $total_duration / 4 * $i"
+            output_file="RC$(printf "%02d" $i).jpg"
+            if [ -f "$output_file" ] && [ $overwrite -eq 0 ]; then
+                echo "File $output_file exists, skipping"
+                continue
+            fi
+            ffmpeg -y -ss $time -i "$file" -frames:v 1 "$output_file"
+        done
+    fi
 done
 
 echo "All images extracted."
