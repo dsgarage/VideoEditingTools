3018747c616f8dcfc799f914a4be4152287b6670
Daisuke Tsukada <dsgarage@gmail.com>
2024-01-07 22:02:15 +0900

    random機能拡張

--- MOVIE2JPG_CUTOUT/MOVIE2JPG_CUTOUT.sh
+++ MOVIE2JPG_CUTOUT/MOVIE2JPG_CUTOUT.sh
@@ -2,6 +2,7 @@
 
 overwrite=0
 mode=""
+declare -a random_times  # randomモードでの切り出し位置を記録する配列
 
 # オプションを取得
 while getopts "rfsoe" option; do
@@ -28,12 +29,20 @@ do
         random)
             for i in {1..4}; do
                 random_time=$((30 + RANDOM % (total_duration - 30)))
+                random_times[$i]=$random_time  # 切り出し位置を配列に記録
                 output_file="RC$(printf "%02d" $i).jpg"
                 if [ -f "$output_file" ] && [ $overwrite -eq 0 ]; then
                     echo "File $output_file exists, skipping"
                     continue
                 fi
                 ffmpeg -y -ss $random_time -i "$file" -frames:v 1 "$output_file"
+            done
+            # 昇順に並べ替えて出力
+            IFS=$'\n' sorted_times=($(sort -n <<<"${random_times[*]}"))
+            unset IFS
+            for i in {1..4}; do
+                index=$(printf "%02d" $i)
+                echo "RC${index}.jpg: ${sorted_times[$i-1]}"
             done
             ;;
 
@@ -54,15 +63,18 @@ do
                     continue
                 fi
                 ffmpeg -y -ss $time -i "$file" -frames:v 1 "$output_file"
+                echo "RC$(printf "%02d" $i).jpg: $time"  # 切り出し位置の出力
             done
             ;;
 
         start_frame)
             ffmpeg -y -ss 1 -i "$file" -frames:v 1 "RC01.jpg"
+            echo "RC01.jpg: 1"  # 切り出し位置の出力
             ;;
 
         end_frame)
             ffmpeg -y -ss $(($total_duration - 1)) -i "$file" -frames:v 1 "RC04.jpg"
+            echo "RC04.jpg: $(($total_duration - 1))"  # 切り出し位置の出力
             ;;
     esac
 done
